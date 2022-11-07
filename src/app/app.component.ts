f142191287cd887328a1c2870e554a05cf5c311e
Felipe Barcon <felipe.barcon@gmail.com>
2022-11-07 11:06:52 +0100

    creating cocktails list template style and logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,4 @@ import { Cocktail } from './interfaces/cocktail.interface';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  cocktail: Cocktail = {
-    name: 'Mojito',
-    img: 'https://lacocotte.net/wp-content/uploads/2021/03/Mojito.jpg',
-    description:
-      'Ce classique cubain peut sembler dépassé à côté de la mode actuelle des cocktails – comme s’il était un héritage des années 2000 – et pourtant il reste un véritable classique, très apprécié des connaisseurs. La fraîcheur de la menthe et de l’eau, l’éclat du rhum, du citron vert et de la cassonade avec de la glace en font toujours une boisson désaltérante.s',
-  };
-}
+export class AppComponent {}
---
+++ src/app/cocktail-list/cocktail-list.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Cocktail } from '../interfaces/cocktail.interface';
+
+@Component({
+  selector: 'app-cocktail-list',
+  templateUrl: './cocktail-list.component.html',
+  styleUrls: ['./cocktail-list.component.scss'],
+})
+export class CocktailListComponent implements OnInit {
+  cocktails: Cocktail[] = [
+    {
+      name: 'Mojito',
+      img: 'https://lacocotte.net/wp-content/uploads/2021/03/Mojito.jpg',
+      description:
+        'Ce classique cubain peut sembler dépassé à côté de la mode actuelle des cocktails – comme s’il était un héritage des années 2000 – et pourtant il reste un véritable classique, très apprécié des connaisseurs. La fraîcheur de la menthe et de l’eau, l’éclat du rhum, du citron vert et de la cassonade avec de la glace en font toujours une boisson désaltérantes',
+    },
+    {
+      name: 'Bellini',
+      img: 'https://maisonfoody.com/sites/default/files/styles/article_paragraph_image/public/2019-11/bellini_1.jpg?itok=IGogglim',
+      description:
+        'Venu tout droit de Norvège l’Aquavit est l’alcool le plus trendy du moment. On l’aime tout particulièrement sous forme de cocktail « Fresh » et pour le réaliser, voici la marche à suivre : mélangez 4cl d’Aquavit, 1cl de Grand Marnier, du jus d’orange et quelques feuilles de menthe. Servez-le accompagné de petits canapés de saumon de Norvège et baies roses et vous voilà avec un combo d’apéro parfait !',
+    },
+    {
+      name: 'Sangria',
+      img: 'https://maisonfoody.com/sites/default/files/styles/article_paragraph_image/public/2019-11/sangria.jpg?itok=LjVLCYpj',
+      description:
+        'Offrez à vos invités un petit détour par l’Espagne avec une Sangria rouge (il existe également des versions blanches et rose). Pour un pichet de 6 personnes, mélangez 1l de vin rouge, 25cl de limonade, 20cl de jus d’orange, 10cl de Cointreau ainsi que deux oranges et un citron jaune coupés en tranches. Ajoutez une gousse de vanille, 50g de sucre en poudre et ½ cuillère à café de cannelle moulue. Et si pour aller un peu plus loin dans le thème vous proposiez à vos invités quelques tranches de jambon Serrano ?',
+    },
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
