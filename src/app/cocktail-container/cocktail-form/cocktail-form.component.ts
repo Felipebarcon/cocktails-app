631d5f5965e4af63e401f2d6ede4ad548093513b
Felipe Barcon <felipe.barcon@gmail.com>
2022-12-22 21:08:45 +0100

    creation formulaire ajouter un cocktail, html et css

--- src/app/cocktail-container/cocktail-form/cocktail-form.component.ts
+++ src/app/cocktail-container/cocktail-form/cocktail-form.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cocktail-form',
   templateUrl: './cocktail-form.component.html',
-  styleUrls: ['./cocktail-form.component.scss']
+  styleUrls: ['./cocktail-form.component.scss'],
 })
 export class CocktailFormComponent implements OnInit {
+  public cocktailForm: FormGroup = this.fb.group({
+    name: ['', Validators.required],
+    img: ['', Validators.required],
+    description: ['', Validators.required],
+  });
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public submit(): void {
+    console.log(this.cocktailForm);
   }
-
 }
